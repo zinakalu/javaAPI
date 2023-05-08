8ff343a04f779e8b0a1567037c0359ce4f4ba19e
zinakalu <zina.kalu@gmail.com>
2023-05-08 10:52:52 -0400

    correction

--- ArrayListmagnet.java
+++ ArrayListmagnet.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ArrayListmagenet{
+public class ArrayListMagnet{
      public static void main(String[] args){
     ArrayList<String> a = new ArrayList<String>();
 
